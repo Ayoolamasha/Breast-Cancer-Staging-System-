b17f6425d06b8b6130c5bddd57c1bd2ca2e4bfd7
Ayoola Masha <30448980+Ayoolamasha@users.noreply.github.com>
2020-09-11 04:03:30 +0100

    made changes to the ui and added splash screen. TODO edit records and better graphics

---
+++ app/src/main/java/com/ayoolamasha/breastcancerstaging/Activities/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.ayoolamasha.breastcancerstaging.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ayoolamasha.breastcancerstaging.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int SPLASH_TIMER = 5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
